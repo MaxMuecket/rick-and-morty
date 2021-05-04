771c6a53e195bcaae06692d601cffadfea4d4059
MaxMuecket <max.muecket@googlemail.com>
2021-05-04 15:18:33 +0200

    💄 Add structure and style it

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
 import './style.css';
 import { createElement } from './utils/elements';
 
-const mainElement = createElement('main', {
-  className: 'main',
+const headerElement = createElement('header', {
+  className: 'header',
   children: [
-    createElement('input', { placeholder: 'Enter name' }),
-    createElement('section', { innerText: 'Results' }),
+    createElement('h1', {
+      className: 'header__title',
+      innerText: 'Rick and Morty',
+    }),
+    createElement('input', {
+      placeholder: 'Enter name',
+      className: 'main__input',
+    }),
   ],
 });
 
-document.querySelector('#app').append(mainElement);
+const mainElement = createElement('main', {
+  className: 'main',
+  children: [
+    createElement('section', {
+      innerText: 'Results',
+      className: 'main__result',
+    }),
+  ],
+});
+
+const footerElement = createElement('footer', {
+  className: 'footer',
+  children: [
+    createElement('a', {
+      href: 'https://rickandmortyapi.com/',
+      className: 'footer__link',
+      innerText: 'API',
+      target: '_blank',
+    }),
+  ],
+});
+
+document
+  .querySelector('#app')
+  .append(headerElement, mainElement, footerElement);
